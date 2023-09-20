95db9d317da3ada84e5786d1e8992c78b23be74a
Joel Perez <perezjoel1994@gmail.com>
2023-09-20 20:38:47 -0300

    Modificacion encontrar id en getProductsById

---
+++ src/index.js
@@ -0,0 +1,91 @@
+class ProductManager {
+  constructor() {
+    this.products = [];
+    this.nextId = 1;
+  }
+
+  addProduct(title, description, code, price, thumbnail, stock) {
+    // Verificar si el código ya existe en la lista de productos
+    const existingProduct = this.products.find(product => product.code === code);
+
+    if (existingProduct) {
+      console.log(`El producto con código ${code} ya existe.`);
+      return;
+    }
+
+    // Agregar el producto con un ID autoincrementable
+    const newProduct = {
+      title,
+      description,
+      id: this.nextId++,
+      price,
+      thumbnail,
+      code,
+      stock,
+    };
+
+    this.products.push(newProduct);
+    console.log(`Producto ${title} agregado con éxito.`);
+  }
+
+  getProductsById() {
+    return this.products;
+  }
+
+
+  
+}
+
+
+const miTienda = new ProductManager();
+miTienda.addProduct("Peli1", "Terror", "ADJ", 3000, "https://www.cinepolis.com.ar/peliculas/la-noche-del-demonio-la-puerta-roja", 10);
+miTienda.addProduct("Peli2", "Animacion", "BKJ", 3000, "https://www.netflix.com/ar/title/81582346", 5);
+miTienda.addProduct("Peli3", "Animacion", "ADJ", 4000, "https://www.netflix.com/ar/title/81582346", 10); // Intento de agregar un producto con el mismo código
+const products = miTienda.getProductsById();
+console.log(products);
+
+function getProductsById (id){
+  const productoEncontrado = products.find(products => products.id ==id);
+  return productoEncontrado || null;
+}
+
+const idBuscado=2;
+const productoEncontrado = getProductsById(idBuscado);
+
+if (productoEncontrado){
+  console.log('Producto encontrado:' , productoEncontrado);
+
+} else {
+  console.log('Producto no encontrado');
+}
+// Función para actualizar un producto por su ID y campo
+function updateProduct(productId, fieldToUpdate, newValue) {
+  // Buscamos el índice del producto con el ID proporcionado
+  const index = products.findIndex(producto => producto.id === productId);
+
+  // Si encontramos el producto
+  if (index !== -1) {
+    // Actualizamos el campo especificado con el nuevo valor
+    products[index][fieldToUpdate] = newValue;
+    console.log(`Producto con ID ${productId} actualizado: ${fieldToUpdate} = ${newValue}`);
+  } else {
+    console.log(`Producto con ID ${productId} no encontrado.`);
+  }
+}
+
+// Uso de la función para actualizar el precio del Producto 2
+updateProduct(2, 'price', 2500);
+
+// Verificamos el estado actual de la lista de productos
+console.log(products);
+
+
+// Ahora, productos solo contendrá los productos con IDs diferentes a 2
+console.log(products);
+
+
+
+
+
+
+
